42425bf2d345c4cfcb7ef55cf5e12cfb305d108b
npd16 <npd16@pitt.edu>
2016-04-14 14:21:50 -0400

    Added Constraints

--- project.sql
+++ project.sql
@@ -17,23 +17,29 @@ CREATE TABLE Profiles
 );
 
 CREATE TABLE Friendships
-(	shipId	number(10) PRIMARY KEY,
-	userId1 number(10),
+(	userId1 number(10),
 	userId2	number(10),
-	status	varchar2(12),
-	date_sent	DATE
+	status	boolean,
+	date_sent	DATE,
+	CONSTRAINT Friendships_PK PRIMARY KEY (userId1,userId2),
+	CONSTRAINT Friendships_FK_Profiles1 FOREIGN KEY (userId1) REFERENCES Profiles(userID),
+	CONSTRAINT Friendships_FK_Profiles2 FOREIGN KEY (userId2) REFERENCES Profiles(userID),
+	CONSTRAINT Check_SameUser CHECK (userId1 != userId2)
 );
 
 CREATE TABLE Groups
 (	groupId	number(10) PRIMARY KEY,
 	gname	varchar2(32),
 	description	varchar2(140),
-	members	number(3)
+	members	number(4),
+	CONSTRAINT Check_Members CHECK (members <= 1000)
 );
 
 CREATE TABLE GroupMembers
 (	groupId	number(10),
-	userId	number(10)
+	userId	number(10),
+	CONSTRAINT GroupMembers_FK_Groups FOREIGN KEY (groupId) REFERENCES Groups(groupId),
+	CONSTRAINT GroupMembers_FK_Profiles FOREIGN KEY (userId) REFERENCES Profiles(userID)
 );
 
 CREATE TABLE Messages
@@ -41,5 +47,7 @@ CREATE TABLE Messages
 	receiver	number(10),
 	subject	varchar2(32),
 	body	varchar2(100),
-	sent_date	TIMESTAMP
+	sent_date	TIMESTAMP,
+	CONSTRAINT Messages_FK_ProfilesS FOREIGN KEY (sender) REFERENCES Profiles(userID),
+	CONSTRAINT Messages_FK_ProfilesR FOREIGN KEY (receiver) REFERENCES Profiles(userID)
 );
